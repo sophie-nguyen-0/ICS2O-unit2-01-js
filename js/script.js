5bbcca687756e87edd3071816b826968b157e771
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-04 18:14:54 +0000

    js and html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Created by:sophie
+// Created on: oct 2020
+//This file contains the JS functions for index.html
+
+/**
+ * This function displays "Hello, World!".
+ * @constructor
+ */
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "<p>hello world!</p>"
+}
